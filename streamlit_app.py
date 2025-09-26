82af5fa994f2da73bb8ce24dd40f42df8f1f6eaa
eduai20250305-ai <eduai20250305@iceu.kr>
2025-09-27 08:34:40 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+if st.button('클릭!'):
+    st.write('버튼이 눌렸습니다!')
